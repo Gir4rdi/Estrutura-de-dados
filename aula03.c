b31fa2028d5bfb3f75dcbb64adbfb4ff9c7d9833
Gir4rdi <luiz.junior4@sou.ufmt.br>
2025-08-05 15:18:03 -0300

    Add files via upload

---
+++ aula03.c
@@ -0,0 +1,77 @@
+// lista
+// fila
+//vetor/array
+// ponteiros
+// alocação dinâmica
+
+// primeira parte da aula
+
+
+// int A;
+// A = 999;
+// int *p;
+// p = &A;
+
+// int *P2 = null;
+// p2 = p;
+// int *P3 = &p;
+
+// *p = 777;
+// *P2 = 55;
+// printf(%p,p);
+
+// printf("%p", &&);
+
+// printf("%d", *p);
+
+
+
+
+
+// Provavelmente sobre ponteiros, verificar isso
+
+
+//*p utilizamos para acessar alguma coisa 
+
+// int vet[10];
+
+// int vet[] = {1,2,3,4,5},
+// malloc(int("tamanho na memoria"));
+
+// int *p = malloc(sizeof(int) *30);
+
+// p[0] = 7;
+// p[2] = 2;
+
+// struct aluno {
+//     int Rga;
+
+//     char nome[50];
+//     char cpf [11];
+// struct aluno *pp = malloc (sizeof(struct aluno) * 50);
+// struct aluno aux;
+// aux.rga = 1234
+// aux.nome = "nome"; 
+// aux.cpf = "777 777 777 11",
+
+// p[0] = aux;
+
+// struct aluno * isa;
+// struct aluno aux;
+// aux.rga= (2,3;);
+// struct (aux.nome, "thiago");
+// struct(aux.cpf, "123...9");
+
+// isa = &aux;
+// printf("%d", aux.rga;
+// printft("%d", isa -> rga);
+// printft("%d"; (isa). Rga);
+    
+
+//listas  - simplesmente encadeadas, e duplamente encadeadas
+// temos o 1 elemento que a lista aponta pra ele
+
+
+// ordenar lista - como posso fazer um bom algoritimo para ordenar uma lista ? R: função para ordenar lista (trocar-nó)(struct nó*n1 , struct nó*n2)
+
+//  
